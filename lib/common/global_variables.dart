88d82a5be78b4a197b2cdd37a0080d25886a93aa
agdaalif <agdaalif@gmail.com>
2023-12-15 18:15:10 +0700

    integration category and product list

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://a877-2404-8000-1028-10fb-b88d-cacd-f576-f61a.ngrok.io';
+  static const baseUrl = 'https://0644-2404-8000-1028-10fb-f06c-5820-2ce2-a342.ngrok.io';
 }
